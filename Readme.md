3af04d44d07cbaac52f3b532ff4902fb3b454979
111590009 <111590009@ntut.edu.tw>
2024-01-31 22:51:37 +0800

    init

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Discord-bot with golang
+
+## How run
+
+```
+go mod tidy
+go run main.go
+
+```
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	_ "github.com/joho/godotenv/autoload"
+)
+
+func main() {
+	// token := os.Getenv("DCToken")
+	token := "ODIxMjk4NzA2OTc2MDc5OTAz.GwuMPe.AIRNOtf2LkxHH0lvbcln0k8fv1fVJQGlbHiB_k"
+
+	// creates a new Discord session
+	dg, err := discordgo.New("Bot " + token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(messageCreate)
+
+	// 只監聽訊息
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	// 開啟連線
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	// If the message is "ping" reply with "Pong!"
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+
+	// If the message is "pong" reply with "Ping!"
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
+}
